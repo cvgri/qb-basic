95dd405b5d87059bd8b6d142ca9489dd622e3998
cvgri <118135126+cvgri@users.noreply.github.com>
2023-08-27 17:04:49 +0300

    Add files via upload

---
+++ StartServer.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\Monster\Desktop\QBCore
+start C:\Users\Monster\Desktop\QBCore\Artifacts\FXServer.exe +execC:\Users\Monster\Desktop\QBCore\server.cfg +set sv_enforceGameBuild 2372 +set svgui_disable true +set onesync on +set onesync_enableInfinity 1
+exit
---
+++ qbcore.sql
@@ -0,0 +1,172 @@
+CREATE DATABASE IF NOT EXISTS `cvgri_qbcore`
+USE `cvgri_qbcore`;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) NOT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `license` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `money` text NOT NULL,
+  `charinfo` text DEFAULT NULL,
+  `job` text NOT NULL,
+  `gang` text DEFAULT NULL,
+  `position` text NOT NULL,
+  `metadata` text NOT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`citizenid`),
+  KEY `id` (`id`),
+  KEY `last_updated` (`last_updated`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `houselocations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `coords` text DEFAULT NULL,
+  `owned` tinyint(2) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `tier` tinyint(4) DEFAULT NULL,
+  `garage` text NOT NULL DEFAULT '{"y":0,"x":0,"h":0,"z":0}',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `apartments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `citizenid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4144 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `player_houses` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `house` varchar(50) NOT NULL,
+  `identifier` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `keyholders` text DEFAULT NULL,
+  `decorations` text DEFAULT NULL,
+  `stash` text DEFAULT NULL,
+  `outfit` text DEFAULT NULL,
+  `logout` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `house` (`house`),
+  KEY `citizenid` (`citizenid`),
+  KEY `identifier` (`identifier`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `license` varchar(255) NOT NULL,
+  `permission` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `license` varchar(50) DEFAULT NULL,
+  `discord` varchar(50) DEFAULT NULL,
+  `ip` varchar(50) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `expire` int(11) DEFAULT NULL,
+  `bannedby` varchar(255) NOT NULL DEFAULT 'LeBanhammer',
+  PRIMARY KEY (`id`),
+  KEY `license` (`license`),
+  KEY `discord` (`discord`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `player_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `iban` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
+
+
+CREATE TABLE IF NOT EXISTS `playerskins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `skin` text NOT NULL,
+  `active` tinyint(2) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB AUTO_INCREMENT=43010 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `player_outfits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `outfitname` varchar(50) NOT NULL,
+  `model` varchar(50) DEFAULT NULL,
+  `skin` text DEFAULT NULL,
+  `outfitId` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `outfitId` (`outfitId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8970 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `gloveboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) DEFAULT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `stashitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) DEFAULT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`stash`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `trunkitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) DEFAULT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`plate`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `license` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `vehicle` varchar(50) DEFAULT NULL,
+  `hash` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(50) NOT NULL,
+  `fakeplate` varchar(50) DEFAULT NULL,
+  `garage` varchar(50) DEFAULT NULL,
+  `fuel` int(11) DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `state` int(11) DEFAULT 1,
+  `depotprice` int(11) NOT NULL DEFAULT 0,
+  `drivingdistance` int(50) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  `balance` int(11) NOT NULL DEFAULT 0,
+  `paymentamount` int(11) NOT NULL DEFAULT 0,
+  `paymentsleft` int(11) NOT NULL DEFAULT 0,
+  `financetime` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`),
+  KEY `citizenid` (`citizenid`),
+  KEY `license` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
